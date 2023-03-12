2b6c3e2a0f8263ec6bc2ea916eae47247034345d
gwilldan <sayhi2godswill@gmail.com>
2023-03-12 22:36:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App bg-slate-50">
+      <h1>HELLO I'M CSS</h1>
+    </div>
+  )
+}
+
+export default App
