396ecf832b5ad4c824d22bbb5becbeb01ee1ded6
gwilldan <sayhi2godswill@gmail.com>
2023-03-14 19:02:56 +0100

    rewrote a more scalable code for topdeals and categories components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,57 +3,25 @@ import {FaAngleRight} from "react-icons/fa"
 import phone from "../assets/phoneImg.png"
 
 function Categories() {
+
+    const list = [1,2,3,4,5,6]
+
   return (
     <div>
         <div className = " grid gap-8  grid-cols-6">
 
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
+            {/* renders the cards throw a map of items on the list */}
 
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
-
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
-
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
-
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
-
-            <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between'>
-                <h1>Phones & Tablets</h1>
-                <div className='flex justify-between items-center '>
-                    <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
-                    <img src={phone} alt="phone"/>
-                </div>
-            </div>
+            { 
+                list.map(() => <div className=' px-4 py-2 bg-white rounded-xl w-[196px] h-[96px] flex flex-col justify-between hover:shadow-md cursor-pointer'>
+                    <h1>Phones & Tablets</h1>
+                    <div className='flex justify-between items-center'>
+                        <div className=' p-1 bg-b2 text-white rounded-full'><FaAngleRight /></div>
+                        <img src={phone} alt="phone"/>
+                    </div>
+                    </div>
+                )
+            }
             
         </div>
     </div>
